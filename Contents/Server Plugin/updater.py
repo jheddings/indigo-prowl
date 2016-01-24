0f24c7a0f9862975c6552eea3ecfa51c6501df66
jheddings <jason@heddings.com>
2016-01-24 08:36:45 -0700

    Stubs for automatic updates.

---
+++ Contents/Server Plugin/updater.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.5
+
+# this module will check for newer releases on a github repo and update accordingly
+
+import httplib, urllib
+
+def update(owner, repo, currentVersion, targetPath):
+    pass
